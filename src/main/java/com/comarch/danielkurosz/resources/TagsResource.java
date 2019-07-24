bb24c4914bd1c1c1faa515dd1202355a9b84acec
DanielVeB <danielkurosz@gmail.com>
2019-07-24 14:54:15 +0200

    change passing clients id from string to list

--- src/main/java/com/comarch/danielkurosz/resources/TagsResource.java
+++ src/main/java/com/comarch/danielkurosz/resources/TagsResource.java
@@ -10,7 +10,6 @@ import io.dropwizard.auth.Auth;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
@@ -29,31 +28,26 @@ public class TagsResource {
 
     @GET
     @Timed
-    @Path("/users/{client_ids}")
+    @Path("/clients")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTags(@Auth UserAuth userAuth,
-                            @PathParam("client_ids") String clientsId) {
+                            @QueryParam("client_id") List<String> clientsId) {
 
         LOGGER.info("get tags");
-
-        clientsId = clientsId.replaceAll("[\"\\[\\]]", "");
-        List<String> listClientID = Arrays.asList(clientsId.split(","));
-
-        HashMap<String, List<UserTagDTO>> tags = tagsService.getTags(listClientID);
-
+        HashMap<String, List<UserTagDTO>> tags = tagsService.getTags(clientsId);
         return Response.ok(tags).build();
     }
 
     @POST
     @Timed
-    @Path("/userid={id}")
+    @Path("/client={client_id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response create(@Auth UserAuth userAuth,
-                           @PathParam("id") String id) {
-        LOGGER.info("create User with id " + id);
+                           @PathParam("client_id") String clientID) {
+        LOGGER.info("create User with id " + clientID);
         try {
-            tagsService.create(id);
-            return Response.ok().build();
+            List<UserTagDTO> tags = tagsService.create(clientID);
+            return Response.ok(tags).build();
         } catch (AppException e) {
             return Response.ok(null).build();
         }
--- src/main/java/com/comarch/danielkurosz/service/TagsService.java
+++ src/main/java/com/comarch/danielkurosz/service/TagsService.java
@@ -4,7 +4,9 @@ import com.comarch.danielkurosz.dao.MongoTagsDAO;
 import com.comarch.danielkurosz.data.UserTagsEntity;
 import com.comarch.danielkurosz.dto.UserTagDTO;
 import com.comarch.danielkurosz.exceptions.AppException;
+import com.comarch.danielkurosz.exceptions.DuplicateIdException;
 import com.comarch.danielkurosz.exceptions.InvalidClientIdException;
+import com.mongodb.DuplicateKeyException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,9 +23,11 @@ public class TagsService {
         this.tagMapper = tagMapper;
     }
 
-    public HashMap<String, List<UserTagDTO>> getTags(List<String> clientsID){
-
+    public HashMap<String, List<UserTagDTO>> getTags(List<String> clientsID) {
         HashMap<String, List<UserTagDTO>> tags = new HashMap<>();
+        if (clientsID == null) {
+            return tags;
+        }
 
         UserTagsEntity userTagsEntity;
         for (String clientID : clientsID) {
@@ -31,7 +35,7 @@ public class TagsService {
                 userTagsEntity = mongoTagsDAO.getUserTagsEntity(UUID.fromString(clientID));
                 tags.put(clientID, tagMapper.mapToTagDTO(userTagsEntity));
             } catch (IndexOutOfBoundsException | IllegalArgumentException ex) {
-                // do nothing
+                // ignore this id
             }
         }
         return tags;
@@ -41,12 +45,16 @@ public class TagsService {
         UUID uuid;
         try {
             uuid = UUID.fromString(id);
-        } catch (IllegalArgumentException ex) {
+        } catch (IllegalArgumentException | NullPointerException ex) {
             throw new InvalidClientIdException();
         }
         UserTagsEntity entity = new UserTagsEntity();
         entity.setClientId(uuid);
-        mongoTagsDAO.create(entity);
+        try {
+            mongoTagsDAO.create(entity);
+        } catch (DuplicateKeyException ex) {
+            throw new DuplicateIdException();
+        }
         return tagMapper.mapToTagDTO(entity);
     }
 
